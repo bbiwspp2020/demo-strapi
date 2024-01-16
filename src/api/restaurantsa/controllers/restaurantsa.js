1512fb9b0509a2843123b57929511f67397589c3
phisitphod.s <phisitphod.sop@cubeofnine.asia>
2024-01-16 23:39:40 +0700

    first commit

---
+++ src/api/restaurantsa/controllers/restaurantsa.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restaurantsa controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::restaurantsa.restaurantsa');
---
+++ src/api/restaurantsa/services/restaurantsa.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restaurantsa service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::restaurantsa.restaurantsa');
